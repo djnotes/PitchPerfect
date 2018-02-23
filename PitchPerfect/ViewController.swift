2f5147f0bcb2ab0110fa51f5da001de931c0c722
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2018-02-23 21:00:13 +0330

    Added NavigationController and a new ViewController

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,11 +31,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        recordingLabel.text = "در حال ضبط..."
+        recordingLabel.text = "Recording..."
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
     
-    @IBAction func stopRecording(_ sender: Any) {
-        recordingLabel.text = "ضبط متوقف شد"
+    @IBAction func  stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
     }
+    @IBOutlet weak var stopRecordingButton: UIButton!
 }
 
